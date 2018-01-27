d24d850482f2cbf97b61f78a552900c74e2e1ccf
bitotassuya <jareansong@gmail.com>
2018-01-27 19:49:06 +0700

    d

---
+++ app/src/main/java/com/example/asus/herbs4health/Commonflow/CommonProduct.java
@@ -0,0 +1,14 @@
+package com.example.asus.herbs4health.Commonflow;
+
+import com.example.asus.herbs4health.Model.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Non on 1/9/2018.
+ */
+
+public class CommonProduct {
+    public static List<Product> productList = new ArrayList<>();
+}
---
+++ app/src/main/java/com/example/asus/herbs4health/Commonflow/Commonflow.java
@@ -0,0 +1,14 @@
+package com.example.asus.herbs4health.Commonflow;
+
+import com.example.asus.herbs4health.Model.Food;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Non on 12/4/2017.
+ */
+
+public class Commonflow {
+    public  static List<Food> foodList = new ArrayList<>();
+}
